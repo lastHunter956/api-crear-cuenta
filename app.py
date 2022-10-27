448cb3af832998245dc541311181457a8746ae15
lasthuneter956 <jvelandia@utb.edu.co>
2022-10-26 20:06:23 -0500

    actualizar 1.2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify #importar librerias
 from flask_mysqldb import MySQL #configuracion de la base de datos
 from requests import post #configuracion de la base de datos
 from sympy import hn1#configuracion de la base de datos
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -24,7 +24,17 @@ def validar_cedula(cedula):
             return False
     except Exception as e:#el except es para que si hay un error no se caiga el programa
         return jsonify({'message': 'error'})#en caso de que haya un error se retorna un mensaje de error
-
+def validar_correo(correo):
+    try:#el try es para que si hay un error no se caiga el programa
+        cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
+        cursor.execute('select Correo from heroku_978ea61906c2949.usuario where Correo = {0}'.format(correo))#se usa para mostrar los datos de la tabla usuario
+        datos = cursor.fetchall()#el fetchall es para que se muestren todos los datos de la consulta
+        if len(datos) > 0:#se crea un ciclo for para que se muestren todos los datos de la consulta
+            return True
+        else:
+            return False
+    except Exception as e:#el except es para que si hay un error no se caiga el programa
+        return jsonify({'message': 'error'})#en caso de que haya un error se retorna un mensaje de error
 @app.route('/')#ruta de la pagina inicial
 def hello_world():#funcion de la pagina inicial
     return 'Hello, World!'
@@ -34,11 +44,11 @@ def hello_world():#funcion de la pagina inicial
 def get_usuario():
     try:#el try es para que si hay un error no se caiga el programa
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
-        cursor.execute('select Cedula, Nombre, Apellido from heroku_978ea61906c2949.usuario')#se usa para mostrar los datos de la tabla usuario
+        cursor.execute('select Cedula, Nombre, Apellido, Correo from heroku_978ea61906c2949.usuario')#se usa para mostrar los datos de la tabla usuario
         datos = cursor.fetchall()#el fetchall es para que se muestren todos los datos de la consulta
         personas = []#se crea una lista vacia donde se guardaran los datos de los vehiculos
         for fila in datos:#se crea un ciclo for para que se muestren todos los datos de la consulta
-            dato = {'Cedula': fila[0], 'Nombre y apellido': fila[1]+" "+ fila[2]}#se crea un diccionario para que se muestren los datos de los usuarios
+            dato = {'Cedula': fila[0], 'Nombre y apellido': fila[1]+" "+ fila[2], 'Correo': fila[3]}#se crea un diccionario para que se muestren los datos de los usuarios
             personas.append(dato)#se agrega los datos de los usuarios a la lista
         return jsonify({'personas': personas, 'message': 'ok'})
     except Exception as e:#el except es para que si hay un error no se caiga el programa
@@ -49,12 +59,13 @@ def post_usuario():
     try:# el try es para que si hay un error no se caiga el programa
         cursor = mysql.connection.cursor()#se usa para conectar con la base de datos
         cedula = request.json['Cedula']#se usa para obtener los datos de la cedula
+        correo = request.json['Correo']#se usa para obtener los datos del correo
         contraseña = request.json['Contraseña']#se usa para obtener los datos de la contraseña
-        if validar_cedula(cedula):#se usa para validar si la cedula ya existe en la base de datos
-            return jsonify({'message': 'esta cedula ya esta creada'})#error
+        if validar_cedula(cedula) and validar_correo(correo):#se usa para validar si la cedula ya existe en la base de datos
+            return jsonify({'message': 'la cuenta o la cedula ya fue registrada'})#error
         else:
             sql = """INSERT INTO heroku_978ea61906c2949.usuario(Cedula, Direccion, Nombre, Apellido, Ciudad, Pais, Contraseña) 
-            VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')""".format(cedula, request.json['Direccion'], request.json['Nombre'], request.json['Apellido'], request.json['Ciudad'], request.json['Pais'], generate_password_hash(contraseña))#se usa para insertar datos en la tabla usuario''])
+            VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')""".format(cedula, request.json['Direccion'], request.json['Nombre'], request.json['Apellido'], request.json['Ciudad'], request.json['Pais'], generate_password_hash(contraseña),correo)#se usa para insertar datos en la tabla usuario''])
             cursor.execute(sql)#se ejecuta la consulta
             mysql.connection.commit()#guardar los cambios
             return jsonify({'message': 'persona añadida'})#se retorna un mensaje de vehiculo añadido
